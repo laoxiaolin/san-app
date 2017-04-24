b1066826c715f27bc8204b977bba82c3e66f790d
“John Lee <lee475002@gmail.com>
2017-04-24 18:31:39 +0800

    增加gulp设置参数

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp     = require('gulp');
+ 
+var cleanCSS = require('gulp-clean-css');
+var imagemin = require('gulp-imagemin');
+var del      = require('del');
+ 
+// 管理路径
+var src_dir = './src',
+    dst_dir = './dist';
+var paths = {
+  html:   src_dir + '*/*.html',
+  css:    src_dir + '*/css/*.css',
+  js:     src_dir + '*/js/*.js',
+  images: src_dir + '*/img/**/*'
+}
+
+// 删除上一次压缩
+gulp.task('clean', function () {
+  return del(dst_dir)
+});
+
+// 处理 HTML：直接拷贝到输出文件夹
+gulp.task('html', function () {
+  return gulp.src(paths.html)
+    .pipe(gulp.dest(dst_dir))
+});
+
+
+// // 处理 APP-HTML：直接拷贝到输出文件夹
+// gulp.task('app-html', function () {
+//   return gulp.src(src_dir + '/app' + '/*.html')
+//     .pipe(gulp.dest(dst_dir+ '/app'))
+// });
+
+gulp.task('default', ['clean', 'html']);
--- src/app/sanzijing/sanzijing.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var SZJ = (function () {
-    function SZJ() {
-    }
-    return SZJ;
-}());
-exports.SZJ = SZJ;
-//# sourceMappingURL=sanzijing.js.map
